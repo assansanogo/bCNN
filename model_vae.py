292cbef7d831f2a6b7986176573ce30eb2a82c8c
assan <assansanogo@gmail.com>
2019-05-23 12:04:40 +0200

    "initial VAE"

--- model_vae.py
+++ model_vae.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import tensorflow as tf
 from keras.layers import Lambda, Input, Dense
 from keras.models import Model
 from keras.datasets import mnist
 from keras.losses import mse, binary_crossentropy
 from keras.utils import plot_model
 from keras import backend as K
+
 
 import numpy as np
 import matplotlib.pyplot as plt
