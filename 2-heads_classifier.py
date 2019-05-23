dcb6f69a161d08f01d531a196e16bec21466a940
assan <assansanogo@gmail.com>
2019-05-23 13:05:36 +0200

    "second modif VAE (relative paths)"

--- 2-heads_classifier.py
+++ 2-heads_classifier.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import tensorflow as tf
-from keras.layers import Lambda, Input, Dense, Concatenate
+from keras.layers import Lambda, Input, Dense, Concatenate, Flatten, Reshape
 from keras.models import Model
 from keras.datasets import mnist
 from keras.losses import mse, binary_crossentropy
@@ -27,11 +27,14 @@ def create_baseline():
     latent_dim =2
     a = Input(shape=(28,28))
     b = Dense(10, input_shape=(28, 28), kernel_initializer='normal', activation='relu')(a)
-
+    b = Flatten()(b)
+    b = Reshape((2,-1))(b)
     model_a = Model(inputs=a, outputs=b)
 
     c = Input(shape=(latent_dim,), name='z_sampling')
-    d = Concatenate()([model_a.output, c])
+    c = Reshape((2,-1))(c)
+
+    d = Concatenate(axis=-1)([model_a.output, c])
     e = Dense(1, kernel_initializer='normal', activation='sigmoid')(d)
 
     model_a = Model(inputs=[model_a.output, c], outputs=e)
